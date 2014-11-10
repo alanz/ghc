81b90cc04a8a42253ad03dd901ae349c6e77dd12
Alan Zimmerman <alan.zimm@gmail.com>
2014-11-10 17:41:49 +0200

    Note reason for trailing whitespace in test file

--- testsuite/tests/ghc-api/annotations/AnnotationTuple.hs
+++ testsuite/tests/ghc-api/annotations/AnnotationTuple.hs
@@ -14,6 +14,7 @@ bar = print $ map (1, "hello"   , 6.5,, [5, 5, 6, 7]) [Just (), Nothing]
 ;
 baz = (1, "hello", 6.5,,) 'a' (Just ())
 }
-
+-- Note: the trailing whitespace in this file is used to check that we
+-- have an annotation for it.
 
 
